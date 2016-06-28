d042a4eec38873c8f359b4c8e62edecf8a7d8601
Roy Wallace <waldo000000@hotmail.com>
2016-06-28 20:11:12 +1000

    Remove async methods

--- RedPillContract/Service References/RedPill/Reference.cs
+++ RedPillContract/Service References/RedPill/Reference.cs
@@ -36,29 +36,17 @@ namespace RedPillContract.RedPill {
         [System.ServiceModel.OperationContractAttribute(Action="http://KnockKnock.readify.net/IRedPill/WhatIsYourToken", ReplyAction="http://KnockKnock.readify.net/IRedPill/WhatIsYourTokenResponse")]
         System.Guid WhatIsYourToken();
         
-        [System.ServiceModel.OperationContractAttribute(Action="http://KnockKnock.readify.net/IRedPill/WhatIsYourToken", ReplyAction="http://KnockKnock.readify.net/IRedPill/WhatIsYourTokenResponse")]
-        System.Threading.Tasks.Task<System.Guid> WhatIsYourTokenAsync();
-        
         [System.ServiceModel.OperationContractAttribute(Action="http://KnockKnock.readify.net/IRedPill/FibonacciNumber", ReplyAction="http://KnockKnock.readify.net/IRedPill/FibonacciNumberResponse")]
         [System.ServiceModel.FaultContractAttribute(typeof(System.ArgumentOutOfRangeException), Action="http://KnockKnock.readify.net/IRedPill/FibonacciNumberArgumentOutOfRangeException" +
             "Fault", Name="ArgumentOutOfRangeException", Namespace="http://schemas.datacontract.org/2004/07/System")]
         long FibonacciNumber(long n);
         
-        [System.ServiceModel.OperationContractAttribute(Action="http://KnockKnock.readify.net/IRedPill/FibonacciNumber", ReplyAction="http://KnockKnock.readify.net/IRedPill/FibonacciNumberResponse")]
-        System.Threading.Tasks.Task<long> FibonacciNumberAsync(long n);
-        
         [System.ServiceModel.OperationContractAttribute(Action="http://KnockKnock.readify.net/IRedPill/WhatShapeIsThis", ReplyAction="http://KnockKnock.readify.net/IRedPill/WhatShapeIsThisResponse")]
         RedPillContract.RedPill.TriangleType WhatShapeIsThis(int a, int b, int c);
-        
-        [System.ServiceModel.OperationContractAttribute(Action="http://KnockKnock.readify.net/IRedPill/WhatShapeIsThis", ReplyAction="http://KnockKnock.readify.net/IRedPill/WhatShapeIsThisResponse")]
-        System.Threading.Tasks.Task<RedPillContract.RedPill.TriangleType> WhatShapeIsThisAsync(int a, int b, int c);
         
         [System.ServiceModel.OperationContractAttribute(Action="http://KnockKnock.readify.net/IRedPill/ReverseWords", ReplyAction="http://KnockKnock.readify.net/IRedPill/ReverseWordsResponse")]
         [System.ServiceModel.FaultContractAttribute(typeof(System.ArgumentNullException), Action="http://KnockKnock.readify.net/IRedPill/ReverseWordsArgumentNullExceptionFault", Name="ArgumentNullException", Namespace="http://schemas.datacontract.org/2004/07/System")]
         string ReverseWords(string s);
-        
-        [System.ServiceModel.OperationContractAttribute(Action="http://KnockKnock.readify.net/IRedPill/ReverseWords", ReplyAction="http://KnockKnock.readify.net/IRedPill/ReverseWordsResponse")]
-        System.Threading.Tasks.Task<string> ReverseWordsAsync(string s);
     }
     
     [System.CodeDom.Compiler.GeneratedCodeAttribute("System.ServiceModel", "4.0.0.0")]
@@ -92,32 +80,16 @@ namespace RedPillContract.RedPill {
             return base.Channel.WhatIsYourToken();
         }
         
-        public System.Threading.Tasks.Task<System.Guid> WhatIsYourTokenAsync() {
-            return base.Channel.WhatIsYourTokenAsync();
-        }
-        
         public long FibonacciNumber(long n) {
             return base.Channel.FibonacciNumber(n);
-        }
-        
-        public System.Threading.Tasks.Task<long> FibonacciNumberAsync(long n) {
-            return base.Channel.FibonacciNumberAsync(n);
         }
         
         public RedPillContract.RedPill.TriangleType WhatShapeIsThis(int a, int b, int c) {
             return base.Channel.WhatShapeIsThis(a, b, c);
         }
         
-        public System.Threading.Tasks.Task<RedPillContract.RedPill.TriangleType> WhatShapeIsThisAsync(int a, int b, int c) {
-            return base.Channel.WhatShapeIsThisAsync(a, b, c);
-        }
-        
         public string ReverseWords(string s) {
             return base.Channel.ReverseWords(s);
-        }
-        
-        public System.Threading.Tasks.Task<string> ReverseWordsAsync(string s) {
-            return base.Channel.ReverseWordsAsync(s);
         }
     }
 }
