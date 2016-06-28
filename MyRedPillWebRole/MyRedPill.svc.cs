78234a1af990fcaf2a5cf2acdaf06bb9ded60658
Roy Wallace <waldo000000@hotmail.com>
2016-06-28 18:35:55 +1000

    Replace IService with IRedPill; stub implementation

---
+++ MyRedPillWebRole/MyRedPill.svc.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Threading.Tasks;
+using MyRedPillWebRole.RedPill;
+
+namespace MyRedPillWebRole
+{
+    public class MyRedPill : IRedPill
+    {
+        public Guid WhatIsYourToken()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Guid> WhatIsYourTokenAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public long FibonacciNumber(long n)
+        {
+            return 42; //tfsbad
+        }
+
+        public Task<long> FibonacciNumberAsync(long n)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TriangleType WhatShapeIsThis(int a, int b, int c)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TriangleType> WhatShapeIsThisAsync(int a, int b, int c)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string ReverseWords(string s)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> ReverseWordsAsync(string s)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
